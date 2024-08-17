de5eece014806626ce4e3e9641224efb1aa67313
ilari1234 <ilari.lehtonen@gmail.com>
2024-08-17 13:45:47 +0300

    1.07

--- 1/log_output/index.js
+++ 1/log_output/index.js
@@ -1,4 +1,6 @@
 const { v4 } = require('uuid');
+const express = require('express');
+const app = express();
 
 const get_uuid =() => {
     const uuid = v4()
@@ -7,8 +9,15 @@ const get_uuid =() => {
     setInterval(() => {
         console.log(`${new Date().toISOString()}:${uuid}`)
     }, 5000)
-
-
 }
+
+app.get('/status', (req, res) => {
+    res.send(`{ "status": "OK", "timestamp": "${new Date().toISOString()}" }`);
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started in port ${PORT}`);
+});
 
 get_uuid()
