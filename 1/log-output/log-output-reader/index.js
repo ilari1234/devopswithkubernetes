b73a897079554e3bc417017c4250e972953b1390
Ilari Lehtonen <ilari.lehtonen@gmail.com>
2024-08-20 21:56:36 +0300

    1.11 node

--- 1/log-output/log-output-reader/index.js
+++ 1/log-output/log-output-reader/index.js
@@ -1,28 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('node:fs');
-const path = require('node:path');
-const crypto = require('crypto');
+const fs = require("node:fs");
+const path = require("node:path");
+const crypto = require("crypto");
 
-const dirPath = path.join(__dirname, 'logs')
-const logfilePath = path.join(dirPath, 'timestamps.log')
+const dirPath = path.join(__dirname, "logs");
+const timestampfilePath = path.join(dirPath, "timestamps.log");
+const pingfilePath = path.join(dirPath, "pingpong.log");
 
-app.get('/status', (req, res) => {
-    res.send(`{ "status": "OK", "timestamp": "${new Date().toISOString()}" }`);
-})
+app.get("/status", (req, res) => {
+  res.send(`{ "status": "OK", "timestamp": "${new Date().toISOString()}" }`);
+});
 
-app.get('/log', async (req, res) => {
-    try {
-        const data = await fs.promises.readFile(logfilePath, 'utf8');
-        const hash = crypto.createHash('sha256').update(data).digest('hex');
-        res.send(`<p>Hash: ${hash}</p><pre>${data}</pre>`);
-    } catch (err) {
-        console.error(`Error reading file: ${err}`);
-        return res.status(500).send('Error reading file');
-    }
-})
+app.get("/timestamps", async (req, res) => {
+  try {
+    const timestamp = new Date().toISOString();
+    const data = await fs.promises.readFile(timestampfilePath, "utf8");
+    const hash = crypto.createHash("sha256").update(data).digest("hex");
+    res.send(`<p>Hash: ${hash}</p><pre>${data}</pre>`);
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+    return res.status(500).send("Error reading file");
+  }
+});
+
+app.get("/log", async (req, res) => {
+  try {
+    const timestamp = new Date().toISOString();
+    const data = await fs.promises.readFile(pingfilePath, "utf8");
+    const hash = crypto.createHash("sha256").update(data).digest("hex");
+    res.send(`<p>${timestamp}: ${hash}</p><pre>${data}</pre>`);
+  } catch (err) {
+    console.error(`Error reading file: ${err}`);
+    return res.status(500).send("Error reading file");
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server started in port ${PORT}`);
+  console.log(`Server started in port ${PORT}`);
 });
--- 1/ping-pong/index.js
+++ 1/ping-pong/index.js
@@ -1,14 +1,35 @@
-const express = require('express');
+const fs = require("node:fs");
+const path = require("node:path");
+const express = require("express");
 const app = express();
 
+const dirPath = path.join(__dirname, "logs");
+const pingfilePath = path.join(dirPath, "pingpong.log");
 let counter = 0;
 
-app.get('/pingpong', (req, res) => {
-    res.send(`counter: ${counter}`);
-    counter++;
-})
+app.get("/pingpong", (req, res) => {
+  res.send(`counter: ${counter}`);
+  counter++;
+  logPing(counter);
+});
+
+const createLogDir = () => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+};
+
+const logPing = (count) => {
+  fs.writeFile(pingfilePath, `Ping / Pongs: ${count}`, (err) => {
+    if (err) {
+      console.error(`Error writing file: ${err}`);
+    }
+  });
+};
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server started in port ${PORT}`);
+  console.log(`Server started in port ${PORT}`);
 });
+
+createLogDir();
