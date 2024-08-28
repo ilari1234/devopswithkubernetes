cf39b52bd6e147242b4a1bc603e919a5ab157a78
ilari1234 <ilari.lehtonen@gmail.com>
2024-08-28 22:39:01 +0300

    1.12

--- 1/todo-application/index.js
+++ 1/todo-application/index.js
@@ -4,18 +4,20 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// Todo: fix the code below
 app.use(express.static('images'))
 
 const dirPath = path.join(__dirname, 'images')
 const imageFilePath = path.join(dirPath, 'image.jpg')
-const imageUrl = 'https://picsum.photos/1200'
+const imageUrl = process.env.IMAGEURL || 'https://picsum.photos/1200'
+const pollInterval = parseInt(process.env.POLLINTERVAL) || 3600000    
 
 const createImageDir = () => {
     if (!fs.existsSync(dirPath)) {
         fs.mkdirSync(dirPath)
     }
 }
+
+const imageExists = (filePath) => fs.existsSync(filePath)
 
 const downloadImage = async (url, outputPath) => {
     try {
@@ -47,7 +49,7 @@ const downloadImage = async (url, outputPath) => {
 
 app.get('/', (req, res) => {
     const html = '<h1>Todo Application</h1> <img src="/imageFilePath" />'
-    res.send(`<h1>Todo Application</h1> <img src="/images/image.jpg" />`);
+    res.send(`<h1>Todo Application</h1> <img src="image.jpg" />`);
 })
 
 const PORT = process.env.PORT || 3000;
@@ -56,4 +58,10 @@ app.listen(PORT, () => {
 });
 
 createImageDir()
-downloadImage(imageUrl, imageFilePath)
+
+if (!imageExists(imageFilePath)) {
+    downloadImage(imageUrl, imageFilePath)
+    setInterval(() => downloadImage(imageUrl, imageFilePath), pollInterval)
+} else {
+    setInterval(() => downloadImage(imageUrl, imageFilePath), pollInterval)
+}
