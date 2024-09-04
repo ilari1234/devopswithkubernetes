fdcd3430554261e097941e4a09137dbe7667564b
ilari1234 <ilari.lehtonen@gmail.com>
2024-09-04 18:50:21 +0300

    2.02

--- 2/todo-application/todo-backend/index.js
+++ 2/todo-application/todo-backend/index.js
@@ -22,7 +22,7 @@ app.post('/todos', (req, res) => {
   res.status(201).json(todo)
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`)
 })
