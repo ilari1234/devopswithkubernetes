e2df231fa1e7345788d32a8932701f3f0395e12a
ilari1234 <ilari.lehtonen@gmail.com>
2024-09-07 22:24:48 +0300

    2.07

--- 2/ping-pong/index.js
+++ 2/ping-pong/index.js
@@ -1,45 +1,73 @@
-const fs = require('node:fs')
-const path = require('node:path')
 const express = require('express')
 const app = express()
 
-const dirPath = path.join(__dirname, 'logs')
-const pingfilePath = path.join(dirPath, 'pingpong.log')
-let counter = 0
+const pg = require('pg')
+const { Pool } = pg
+const pool = new Pool({
+  host : process.env.POSTGRES_HOST,
+  user : process.env.POSTGRES_USER,
+  password : process.env.POSTGRES_PASSWORD,
+  database : process.env.POSTGRES_DB, }
+)
 
-app.get('/pingpong', (req, res) => {
-  counter++
-  res.send(`counter: ${counter}`)
+app.get('/pingpong', async (req, res) => {
+  await incrementPing()
+  const counter = await loadPing()
+  res.send(`Counter: ${counter}`)
 })
 
-app.get('/pongs', (req, res) => {
+app.get('/pongs', async (req, res) => {
+  const counter = await loadPing()
   res.send(`Ping / Pongs: ${counter}`)
 })
 
-const createLogDir = () => {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath)
+const initTable = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS pingpong (
+        id SERIAL PRIMARY KEY,
+        count INT
+      )
+    `)
+  } catch (error) {
+    console.error(`Error creating table: ${error.stack}`)
   }
 }
 
-const logPing = (count) => {
-  fs.writeFile(pingfilePath, `Ping / Pongs: ${count}`, (err) => {
-    if (err) {
-      console.error(`Error writing file: ${err}`)
+const initCounter = async () => {
+  try {
+    const result = await pool.query('SELECT * FROM pingpong')
+    if (result.rows.length === 0) {
+      await pool.query('INSERT INTO pingpong (count) VALUES (0)')
     }
-  })
-}
-
-const updateCounterFromFile = () => {
-  if (fs.existsSync(pingfilePath)) {
-    fs.readFile(pingfilePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(`Error reading file: ${err}`)
-      }
-      counter = parseInt(data.split(':')[1].trim())
-    })
+  } catch (error) {
+    console.error(`Error initializing ping: ${error.stack}`)
   }
 }
+
+const initDatabase = async () => {
+  await initTable()
+  await initCounter()
+}
+
+const incrementPing = async () => {
+  try {
+    await pool.query('UPDATE pingpong SET count = count + 1 WHERE id = 1')
+  } catch (error) {
+    console.error(`Error incrementing ping: ${error.stack}`)
+  }
+}
+
+const loadPing = async () => {
+  try {
+    const result = await pool.query('SELECT count FROM pingpong WHERE id = 1')
+    return result.rows[0].count
+  } catch (error) {
+    console.error(`Error loading ping: ${error.stack}`)
+  }
+}
+
+initDatabase()
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
