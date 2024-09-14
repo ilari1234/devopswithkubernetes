c05bb9745dacb5b49c211b93f19c52b06027b25c
ilari1234 <ilari.lehtonen@gmail.com>
2024-09-14 15:24:31 +0300

    2.10

--- 2/todo-application/todo-backend/index.js
+++ 2/todo-application/todo-backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
@@ -11,8 +12,21 @@ const pool = new Pool({
   database : process.env.POSTGRES_DB, }
 )
 
+morgan.token('postBody', (req) => {
+  if (req.method==='POST') {
+    return(JSON.stringify(req.body))
+  }
+})
+
+morgan.token('postError', (req) => {
+  if (req.method==='POST' && req.body.title.length > 140) {
+    return('Title length should be maximum 140 characters')
+  }
+})
+
 app.use(express.json())
 app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postError :postBody'))
 
 const loadTodos = async () => {
   const { rows } = await pool.query('SELECT * FROM todos')
@@ -30,8 +44,12 @@ app.get('/todos', async (req, res) => {
 })
 
 app.post('/todos', async (req, res) => {
-  const todo = await saveTodo(req.body)
-  res.status(201).json(todo)
+  if (req.body.title.length > 140) {
+    return res.status(400).json({ error: 'Title length should be maximum 140 characters' })
+  } else {
+    const todo = await saveTodo(req.body)
+    res.status(201).json(todo)
+  }
 })
 
 const PORT = process.env.PORT || 3001
